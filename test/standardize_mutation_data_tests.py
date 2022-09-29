ac3ad3806b7e7bd44d3297beb3190726b9dc96d9
Pieter Lukasse <pieterlukasse@gmail.com>
2022-09-29 13:48:15 +0200

    update tests and move to test folder

--- test/standardize_mutation_data_tests.py
+++ test/standardize_mutation_data_tests.py
@@ -69,6 +69,8 @@ class StandardizeMutationDataTests(unittest.TestCase):
         self.assertEqual("Expected max 2 sample columns for tumor and normal sample. But found 3 columns.", str(exc.exception))
 
     def test_extract_vcf_data_from_file_tumor_normal_swap(self):
+        # this test is just showing that the name of the columns does not matter...it will be the order
+        # that matters in this case: first column is seen as Tumor_Sample_Barcode, second column as Matched_Norm_Sample_Barcode:
         _, vcf = tempfile.mkstemp()
         with open(vcf, 'w') as f:
            f.write(
@@ -78,8 +80,8 @@ class StandardizeMutationDataTests(unittest.TestCase):
         maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
         self.assertEqual(1, len(maf_data))
         maf_row = maf_data[0]
-        self.assertNotEqual('NORMAL', maf_row['Tumor_Sample_Barcode'])
-        self.assertEqual('NORMAL', maf_row['Matched_Norm_Sample_Barcode'])
+        self.assertEqual('NORMAL', maf_row['Tumor_Sample_Barcode'])
+        self.assertEqual('TUMOR', maf_row['Matched_Norm_Sample_Barcode'])
 
     def test_extract_vcf_data_from_file_1_normal_sample(self):
         _, vcf = tempfile.mkstemp()
@@ -99,6 +101,23 @@ class StandardizeMutationDataTests(unittest.TestCase):
             '##normal_sample=S1\n'
             '##tumor_sample=S2\n'
             "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tS1\tS2\n"
+            "20\t14370\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\t1|0:48:8:51,51\n"
+           )
+        maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
+        self.assertEqual(1, len(maf_data))
+        maf_row = maf_data[0]
+        self.assertEqual('S2', maf_row['Tumor_Sample_Barcode'])
+        self.assertEqual('S1', maf_row['Matched_Norm_Sample_Barcode'])
+
+    def test_extract_vcf_data_from_file_2_samples_specified_in_header_reversed_order(self):
+        # same as above, but with column order reversed in CHROM header line...should still give the same result, just
+        # to show that it is really reading from header and not from column order:
+        _, vcf = tempfile.mkstemp()
+        with open(vcf, 'w') as f:
+           f.write(
+            '##normal_sample=S1\n'
+            '##tumor_sample=S2\n'
+            "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tS2\tS1\n"
             "20\t14370\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\t1|0:48:8:51,51\n"
            )
         maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
@@ -133,34 +152,6 @@ class StandardizeMutationDataTests(unittest.TestCase):
             extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
         self.assertEqual("There is tumor_sample=S2 in the header, but no respective column found.", str(exc.exception))
 
-    def test_extract_vcf_data_from_file_tumor_sample_out_of_2_samples_specified_in_header(self):
-        _, vcf = tempfile.mkstemp()
-        with open(vcf, 'w') as f:
-           f.write(
-            '##tumor_sample=S2\n'
-            "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tS1\tS2\n"
-            "20\t14370\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\t1|0:48:8:51,51\n"
-           )
-        maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
-        self.assertEqual(1, len(maf_data))
-        maf_row = maf_data[0]
-        self.assertEqual('S2', maf_row['Tumor_Sample_Barcode'])
-        self.assertEqual('S1', maf_row['Matched_Norm_Sample_Barcode'])
-
-    def test_extract_vcf_data_from_file_normal_sample_out_of_2_samples_specified_in_header(self):
-        _, vcf = tempfile.mkstemp()
-        with open(vcf, 'w') as f:
-           f.write(
-            '##normal_sample=S1\n'
-            "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tS1\tS2\n"
-            "20\t14370\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\t1|0:48:8:51,51\n"
-           )
-        maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
-        self.assertEqual(1, len(maf_data))
-        maf_row = maf_data[0]
-        self.assertEqual('S2', maf_row['Tumor_Sample_Barcode'])
-        self.assertEqual('S1', maf_row['Matched_Norm_Sample_Barcode'])
-
     def test_extract_vcf_data_from_file_header_samples_have_precedence_over_the_rest_of_strategies(self):
         _, vcf = tempfile.mkstemp()
         with open(vcf, 'w') as f:
@@ -184,11 +175,9 @@ class StandardizeMutationDataTests(unittest.TestCase):
             "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tA=B\n"
             "20\t14370\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\n"
            )
-        maf_data = extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
-        self.assertEqual(1, len(maf_data))
-        maf_row = maf_data[0]
-        self.assertEqual('A=B', maf_row['Tumor_Sample_Barcode'])
-        self.assertEqual('NORMAL', maf_row['Matched_Norm_Sample_Barcode'])
+        with self.assertRaises(Exception) as exc:
+            extract_vcf_data_from_file(vcf, 'center name 1', 'sequence source 1')
+        self.assertIn("The tumor_sample and normal_sample are expected together", str(exc.exception))
 
 if __name__=='__main__':
     unittest.main()
